fdcfec17444895123481f1e7116c2e5113b93b29
Peace Amudipe <peaceamudipe@gmail.com>
2024-09-03 12:43:40 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -2,6 +2,9 @@ let x = 5
 let operator = "add"
 let y = 3
 const calculator = (x, operator, y) =>{
+    if (typeof "x" !== "number" && typeof "y" !== "number"){
+        return "Both inputs must be digits."
+    }
     if (operator === "add"){
         return (x + y)
     } else if (operator === "subtract"){
@@ -24,3 +27,4 @@ console.log(calculator(12, "subtract", 4));
 console.log(calculator(5, "multiply", 7 ));
 console.log(calculator(9, "divide", 3))
 console.log(calculator(4, "divide", 0))
+console.log(calculator(5, "multiply", "one"))
